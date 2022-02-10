f882bbb96e3530f319dafb39e1040b0d0adf3f72
soysofia <20170194@ulsavictoria.edu.mx>
2022-02-10 12:59:56 -0600

    attack slime

--- Assets/Scripts/Slime.cs
+++ Assets/Scripts/Slime.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
 public class Slime : MonoBehaviour
 {
 
@@ -18,6 +21,29 @@ public class Slime : MonoBehaviour
     float patrolTimeLimit =3f;
     float horizontalDirection =1.0f;
 
+    float idlingTime =2f;
+
+    IEnumerator patroling;
+    IEnumerator idling;
+    IEnumerator lastRoutine;
+
+    IEnumerator attack;
+
+    [SerializeField, Range(0.1f,5f)]
+    float rayDistance = 2f;
+
+    [SerializeField]
+    Color rayColor =Color.red;
+
+    [SerializeField]
+    LayerMask playerLayer;
+
+    [SerializeField]
+    Vector3 rayOrigin;
+
+    [SerializeField]
+    AnimationClip attackClip;
+
 
 
     void Awake()
@@ -31,11 +57,103 @@ public class Slime : MonoBehaviour
     void Start()
     {
         
+        StartIA();
+    }
+
+    IEnumerator IdlingRoutine()
+    
+    {
+        anim.SetBool("patrol",false);
+        yield return new WaitForSeconds(idlingTime);
+        StartPatrolling();
+
     }
 
 
+
+    void StartAttack()
+    {
+        attack = AttackingRoutine();
+        StartCoroutine(attack);
+    }
+
+    IEnumerator AttackingRoutine()
+    {
+        anim.SetTrigger("attack");
+        yield return new WaitForSeconds(attackClip.length);
+        StartCoroutine(lastRoutine);
+    }
+
+    IEnumerator PatrolingRoutine()
+    {
+
+        while (true) 
+        {
+            if(Attack)
+            {
+                lastRoutine= patroling;
+                StartAttack();
+              yield break;
+
+            }
+            Debug.Log("patroling");
+            rb2D.position += direction * moveSpeed* Time.deltaTime;
+            timer+= Time.deltaTime;
+
+
+            if (timer>= patrolTimeLimit)
+             {   
+                 
+                StartIdling();
+
+                yield break;
+
+            }
+            yield return null;
+
+        }
+    }
+
+ 
+
+
+    void StartIdling()
+    {
+
+        idling = IdlingRoutine();
+        StartCoroutine(idling);
+        
+        
+    }
+
+    void StartPatrolling()
+
+    {  
+        anim.SetBool("patrol", true);
+        timer = 0f;
+        direction = direction == Vector2.right ? Vector2.left: Vector2.right;
+        spr.flipX =FlipSpriteX;
+        anim.SetBool("patrol",true);
+        patroling =PatrolingRoutine();
+        StartCoroutine(patroling);
+    }
+
+
+    void StartIA()
+    {
+        patroling = PatrolingRoutine();
+        StartCoroutine(patroling);
+    }
+
+    void Update()
+    {
+        if (Attack)
+        {
+            Debug.Log("attack");
+        }
+    }
     
-    void FixedUpdate()
+    /*void FixedUpdate()
     {
         rb2D.position += direction * moveSpeed* Time.deltaTime;
         timer+= Time.deltaTime;
@@ -49,6 +167,22 @@ public class Slime : MonoBehaviour
         }
 
     }
-
+*/
     bool FlipSpriteX => direction == Vector2.right ? false : true;
+
+    bool Attack => Physics2D.Raycast(transform.position+rayOrigin,Vector2.right, rayDistance, playerLayer) || 
+    Physics2D.Raycast(transform.position+rayOrigin,Vector2.left, rayDistance, playerLayer);
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color =rayColor;
+        Gizmos.DrawRay(transform.position + rayOrigin,Vector3.right *rayDistance);
+        Gizmos.DrawRay(transform.position+ rayOrigin, Vector3.left*rayDistance);
+
+    }
+
+
+
+
+
 }
